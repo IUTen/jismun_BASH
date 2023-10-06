e1328645f59d9f3da6657e59e253787fa7f86b71
jismun <144932790+jismun@users.noreply.github.com>
2023-10-06 14:52:26 +0300

    Update BASH_lab2.sh

--- lab2/BASH_lab2.sh
+++ lab2/BASH_lab2.sh
@@ -6,15 +6,19 @@ ext=$2
 list=(0 1 2 3 4 5 6 7 8 9)
 
 for file in $mydir/*
-do
-	for num in list
-	do 
-		if [ $(head -c1 $file) == $num ] ;
+do	
+	for num in ${list[@]}
+	do
+		if [ $( head -c1 $file ) == $num ]  ;
 		then
 			mv $file $file$ext
+			break
 		fi
 	done
+ 
+	if [ $num != 9 ] ;
+	then
+		break
+	fi
 done
-
-
 
